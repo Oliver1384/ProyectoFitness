e991b29d0e46679bcd2a040f08d0063998b2aac3
Oliver1384 <oliver_artiles@hotmail.com>
2022-02-10 12:49:39 +0000

    implementando podcast

--- resources/views/podcasts.blade.php
+++ resources/views/podcasts.blade.php
@@ -1,5 +1,39 @@
 @extends('layouts.template')
 
 @section('content')
-Podcasts
+    <div>
+        <h1>PODCOSTS</h1>
+        <div>
+            <img src="{{ asset('images/logo/navbar-logo_110x50.png') }}" alt="">
+            <div>
+                <h2>PODCAST 1</h2>
+                <h3>TEMA PODCAST</h3>
+            </div>
+        </div>
+        <div>
+            <img src="" alt="">
+            <div>
+                <h2>PODCAST 1</h2>
+                <h3>TEMA PODCAST</h3>
+            </div>
+        </div>
+        <div>
+            <img src="" alt="">
+            <div>
+                <h2>PODCAST 1</h2>
+                <h3>TEMA PODCAST</h3>
+            </div>
+        </div>
+        <div>
+            <img src="" alt="">
+            <div>
+                <h2>PODCAST 1</h2>
+                <h3>TEMA PODCAST</h3>
+            </div>
+        </div>
+    </div>
+    <div>
+        <button><</button>
+        <button>></button>
+    </div>
 @endsection
