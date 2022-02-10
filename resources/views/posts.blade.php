eeb716c320d91ec8e6cfeb02d4de3f3f713d8bdd
borjauni <borjauni@hotmail.com>
2022-02-10 14:56:31 +0000

    Articles Update

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,134 @@
 @extends('layouts.template')
 
 @section('content')
-Posts
-@endsection
+    <div id="articles-page">
+        <div class="row">
+            <div class="col-12">
+                <div id="main-article" class="text-main-white bg-main-orange">
+                    <div class="article-effect" style="background:url('images/articles/Article_Main.png');"></div>
+                    <div class="article-content">
+                        <h1 class="article-title">
+                            <span class="text-main-black">Artículo</span> <span class="text-main-white">principal</span>
+                        </h1>
+                        <div class="article-body">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec arcu orci. In turpis ante,
+                            porta ac cursus a, tempor et leo. Vivamus mattis leo non eros iaculis, nec posuere tellus
+                            mattis. Cras placerat est in ante luctus, ut commodo ex egestas.
+                        </div>
+                        <div class="article-button">
+                            <button class="btn btn-light">Leer más</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 article">
+                <div class="text-main-white bg-main-black">
+                    <div class="article-effect" style="background:url('images/articles/Article_01.png');"></div>
+                    <div class="article-content">
+                        <h1 class="article-title">
+                            <span class="text-main-white">Artículo 1</span>
+                        </h1>
+                        <div class="article-body">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec arcu orci. In turpis ante,
+                            porta ac cursus a, tempor et leo. Vivamus mattis leo non eros iaculis, nec posuere tellus
+                            mattis. Cras placerat est in ante luctus, ut commodo ex egestas.
+                        </div>
+                        <div class="article-button">
+                            <button class="btn btn-light">Leer más</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 article">
+                <div class="text-main-white bg-main-white">
+                    <div class="article-effect" style="background:url('images/articles/Article_02.png');"></div>
+                    <div class="article-content">
+                        <h1 class="article-title">
+                            <span class="text-main-black">Artículo 2</span>
+                        </h1>
+                        <div class="article-body">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec arcu orci. In turpis ante,
+                            porta ac cursus a, tempor et leo. Vivamus mattis leo non eros iaculis, nec posuere tellus
+                            mattis. Cras placerat est in ante luctus, ut commodo ex egestas.
+                        </div>
+                        <div class="article-button">
+                            <button class="btn btn-light">Leer más</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 article">
+                <div class="text-main-white bg-main-white">
+                    <div class="article-effect" style="background:url('images/articles/Article_03.png');"></div>
+                    <div class="article-content">
+                        <h1 class="article-title">
+                            <span class="text-main-black">Artículo 3</span>
+                        </h1>
+                        <div class="article-body">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec arcu orci. In turpis ante,
+                            porta ac cursus a, tempor et leo. Vivamus mattis leo non eros iaculis, nec posuere tellus
+                            mattis. Cras placerat est in ante luctus, ut commodo ex egestas.
+                        </div>
+                        <div class="article-button">
+                            <button class="btn btn-light">Leer más</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 article">
+                <div class="text-main-white bg-main-black">
+                    <div class="article-effect" style="background:url('images/articles/Article_04.png');"></div>
+                    <div class="article-content">
+                        <h1 class="article-title">
+                            <span class="text-main-white">Artículo 4</span>
+                        </h1>
+                        <div class="article-body">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec arcu orci. In turpis ante,
+                            porta ac cursus a, tempor et leo. Vivamus mattis leo non eros iaculis, nec posuere tellus
+                            mattis. Cras placerat est in ante luctus, ut commodo ex egestas.
+                        </div>
+                        <div class="article-button">
+                            <button class="btn btn-light">Leer más</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 article">
+                <div class="text-main-white bg-main-black">
+                    <div class="article-effect" style="background:url('images/articles/Article_05.png');"></div>
+                    <div class="article-content">
+                        <h1 class="article-title">
+                            <span class="text-main-white">Artículo 5</span>
+                        </h1>
+                        <div class="article-body">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec arcu orci. In turpis ante,
+                            porta ac cursus a, tempor et leo. Vivamus mattis leo non eros iaculis, nec posuere tellus
+                            mattis. Cras placerat est in ante luctus, ut commodo ex egestas.
+                        </div>
+                        <div class="article-button">
+                            <button class="btn btn-light">Leer más</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 article">
+                <div class="text-main-white bg-main-white">
+                    <div class="article-effect" style="background:url('images/articles/Article_06.png');"></div>
+                    <div class="article-content">
+                        <h1 class="article-title">
+                            <span class="text-main-black">Artículo 6</span>
+                        </h1>
+                        <div class="article-body">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec arcu orci. In turpis ante,
+                            porta ac cursus a, tempor et leo. Vivamus mattis leo non eros iaculis, nec posuere tellus
+                            mattis. Cras placerat est in ante luctus, ut commodo ex egestas.
+                        </div>
+                        <div class="article-button">
+                            <button class="btn btn-light">Leer más</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+        @endsection
