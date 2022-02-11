ad58f1e68cbdb915d527c877e02dd0ed6b2267e2
AriadnaMiranda95 <77674556+AriadnaMiranda95@users.noreply.github.com>
2022-02-11 12:18:13 +0000

    prueba

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 <div class="contacto-container">
-    <h1>SOBRE NOSOTROS</h1>
+    <h1><span class="sobre">SOBRE </span><span class="nosotros">NOSOTROS</span></h1>
 
 </div>
 Contacto
