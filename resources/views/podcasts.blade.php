eb477bf5f6cd17ef819850ee0a95e97ca4990c1c
oliver <oliver_artiles@hotmail.com>
2022-02-15 20:31:35 +0000

    agregado paginador para escritorio, podcasts

--- resources/views/podcasts.blade.php
+++ resources/views/podcasts.blade.php
@@ -399,4 +399,20 @@
         <button><</button>
         <button>></button>
     </div>
+    <div id="paginator" class="paginadorEscritorio ocultarMovil">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link active" href="#">«</a></li>
+                <li class="page-item"><a class="page-link active" href="#"><</a></li>
+                <li class="page-item"><a class="page-link active" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">4</a></li>
+                <li class="page-item"><a class="page-link" href="#">5</a></li>
+                <li class="page-item"><a class="page-link" href="#">6</a></li>
+                <li class="page-item"><a class="page-link active" href="#">></a></li>
+                <li class="page-item"><a class="page-link active" href="#">»</a></li>
+            </ul>
+        </nav>
+    </div>
 @endsection
