a2b59d0fcbe3da7a8af627318fe75b9337c2a358
Adayelgithub <Aday_el_@outlook.com>
2022-02-17 13:03:48 +0000

    Paso de css a plans.css
    Cambios a plans.css

--- resources/views/plans.blade.php
+++ resources/views/plans.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 
-
-    <div class="container-fluid mt-4" style="background: var(--main-orange);">
+    <link rel="stylesheet" href="{{ URL::asset('css/plans.css') }}">
+    <div class="container-fluid mt-4 planContenido1" >
         <div class="row d-flex justify-content-center">
-            <h1 class="text-center fw-bold fs-1 mt-3">  <span style=" color:var(--main-black)">Elige </span> <span style=" color:var(--main-white)">tu plan </span> </h1>
+            <h1 class="text-center fw-bold fs-1 mt-3">  <span>Elige </span> <span class="span2Contenido1">tu plan </span> </h1>
             <div class="col-6">
-                <p class="fst-italic" style="color: var(--main-black)">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem blanditiis corporis deleniti ea eos et fugit in,
+                <p class="fst-italic pContenido1" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem blanditiis corporis deleniti ea eos et fugit in,
                     iure neque nulla numquam odio officiis perspiciatis quas quis sed sint sunt voluptates. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, illum ipsa magni necessitatibus neque pariatur quidem!
                     Illo ipsum minus neque veritatis vero? Assumenda corporis impedit libero molestias nemo rerum vero. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aut autem debitis dolorem eius, exercitationem ipsa molestiae,
                     mollitia nisi non officia pariatur quidem reiciendis sit sunt velit voluptatum. Dignissimos, vel! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet atque autem culpa cum cupiditate distinctio dolores enim et,
@@ -20,17 +20,17 @@
         </div>
     </div>
 
-    <div class="container-fluid" style="background: var(--main-white);">
+    <div class="container-fluid planContenido2">
         <div class="row d-flex justify-content-center">
-            <h1 class="text-center fw-bold fs-1 mt-3">  <span style=" color:var(--main-black)">Nuestros </span> <span style=" color:var(--main-orange)">planes </span> </h1>
+            <h1 class="text-center fw-bold fs-1 mt-3">  <span>Nuestros </span> <span class="span2Contenido2">planes </span> </h1>
 
 
             <!--Carousel Wrapper-->
-            <div id="multi-item-example" class="carousel slide carousel-multi-item mb-3 d-flex justify-content-center" data-bs-ride="carousel">
+            <div id="multi-item-example" class="carousel slide  carousel-multi-item mb-3 d-flex justify-content-center"  data-bs-interval="false"  data-interval="false" data-bs-ride="carousel">
 
 
                 <a class="d-flex align-items-center" href="#multi-item-example" data-slide="prev">
-                <button class="rounded-circle border border-dark" type="button"   data-bs-slide="prev" style="background-color:var(--main-orange) ">
+                <button class="rounded-circle border border-dark" type="button"   data-bs-slide="prev" >
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Previous</span>
                 </button>
@@ -42,12 +42,12 @@
                     <!--First slide-->
                     <div class="carousel-item active">
                         <div class="row d-flex justify-content-center">
-                            <div class="col-md-3 ">
+                            <div class="col-xl-3 col-md-6 ">
                                 <div class="card mb-2 border border-dark">
                                     <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
                                          alt="Card image cap">
                                     <div class="card-body ">
-                                        <h1 class="card-title text-center" style="color:var(--main-black)">Programa 1</h1>
+                                        <h1 class="card-title text-center" >Programa 1</h1>
                                         <p class="card-text fst-italic p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ex
                                             ercitationem ipsam magni molestiae necessitatibus nisi, placeat repellat suscipit veritatis voluptas?
                                             Ab, architecto at consequatur dolorem eligendi incidunt laborum pariatur placeat?</p>
@@ -56,12 +56,12 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-3 clearfix d-none d-md-block">
-                                <div class="card mb-2 border border-dark" style=" color: var(--main-white); background-color: var(--main-black)">
+                            <div class="col-xl-3 clearfix col-md-6 d-md-block clearfix d-none d-xl-block">
+                                <div class="card mb-2 border border-dark">
                                     <img class="card-img-top" style="" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
                                          alt="Card image cap">
                                     <div class="card-body">
-                                        <h1 class="card-title text-center" style="color:var(--main-white)">Programa 2</h1>
+                                        <h1 class="card-title text-center" >Programa 2</h1>
                                         <p class="card-text fst-italic p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ex
                                             ercitationem ipsam magni molestiae necessitatibus nisi, placeat repellat suscipit veritatis voluptas?
                                             Ab, architecto at consequatur dolorem eligendi incidunt laborum pariatur placeat?</p>
@@ -70,12 +70,12 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-3 clearfix d-none d-md-block">
+                            <div class="col-xl-3 clearfix d-none col-md-6 d-md-block clearfix d-none d-xl-block">
                                 <div class="card mb-2 border border-dark">
                                     <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg"
                                          alt="Card image cap">
                                     <div class="card-body">
-                                        <h1 class="card-title text-center" style="color:var(--main-black)">Programa 3</h1>
+                                        <h1 class="card-title text-center" >Programa 3</h1>
                                         <p class="card-text  fst-italic p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ex
                                             ercitationem ipsam magni molestiae necessitatibus nisi, placeat repellat suscipit veritatis voluptas?
                                             Ab, architecto at consequatur dolorem eligendi incidunt laborum pariatur placeat?</p>
@@ -96,7 +96,7 @@
                                     <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
                                          alt="Card image cap">
                                     <div class="card-body ">
-                                        <h1 class="card-title text-center" style="color:var(--main-black)">Programa 4</h1>
+                                        <h1 class="card-title text-center">Programa 4</h1>
                                         <p class="card-text fst-italic p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ex
                                             ercitationem ipsam magni molestiae necessitatibus nisi, placeat repellat suscipit veritatis voluptas?
                                             Ab, architecto at consequatur dolorem eligendi incidunt laborum pariatur placeat?</p>
@@ -106,11 +106,11 @@
                             </div>
 
                             <div class="col-md-3 clearfix d-none d-md-block">
-                                <div class="card mb-2 border border-dark" style=" color: var(--main-white); background-color: var(--main-black)">
+                                <div class="card mb-2 border border-dark" >
                                     <img class="card-img-top" style="" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
                                          alt="Card image cap">
                                     <div class="card-body">
-                                        <h1 class="card-title text-center" style="color:var(--main-white)">Programa 5</h1>
+                                        <h1 class="card-title text-center" >Programa 5</h1>
                                         <p class="card-text fst-italic p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ex
                                             ercitationem ipsam magni molestiae necessitatibus nisi, placeat repellat suscipit veritatis voluptas?
                                             Ab, architecto at consequatur dolorem eligendi incidunt laborum pariatur placeat?</p>
@@ -124,7 +124,7 @@
                                     <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg"
                                          alt="Card image cap">
                                     <div class="card-body">
-                                        <h1 class="card-title text-center" style="color:var(--main-black)">Programa 6</h1>
+                                        <h1 class="card-title text-center" >Programa 6</h1>
                                         <p class="card-text  fst-italic p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ex
                                             ercitationem ipsam magni molestiae necessitatibus nisi, placeat repellat suscipit veritatis voluptas?
                                             Ab, architecto at consequatur dolorem eligendi incidunt laborum pariatur placeat?</p>
@@ -144,7 +144,7 @@
                                     <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
                                          alt="Card image cap">
                                     <div class="card-body ">
-                                        <h1 class="card-title text-center" style="color:var(--main-black)">Programa 7</h1>
+                                        <h1 class="card-title text-center" >Programa 7</h1>
                                         <p class="card-text fst-italic p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ex
                                             ercitationem ipsam magni molestiae necessitatibus nisi, placeat repellat suscipit veritatis voluptas?
                                             Ab, architecto at consequatur dolorem eligendi incidunt laborum pariatur placeat?</p>
@@ -154,11 +154,11 @@
                             </div>
 
                             <div class="col-md-3 clearfix d-none d-md-block">
-                                <div class="card mb-2 border border-dark" style=" color: var(--main-white); background-color: var(--main-black)">
+                                <div class="card mb-2 border border-dark" >
                                     <img class="card-img-top" style="" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
                                          alt="Card image cap">
                                     <div class="card-body">
-                                        <h1 class="card-title text-center" style="color:var(--main-white)">Programa 8</h1>
+                                        <h1 class="card-title text-center" >Programa 8</h1>
                                         <p class="card-text fst-italic p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ex
                                             ercitationem ipsam magni molestiae necessitatibus nisi, placeat repellat suscipit veritatis voluptas?
                                             Ab, architecto at consequatur dolorem eligendi incidunt laborum pariatur placeat?</p>
@@ -172,7 +172,7 @@
                                     <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg"
                                          alt="Card image cap">
                                     <div class="card-body">
-                                        <h1 class="card-title text-center" style="color:var(--main-black)">Programa 9</h1>
+                                        <h1 class="card-title text-center" >Programa 9</h1>
                                         <p class="card-text  fst-italic p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ex
                                             ercitationem ipsam magni molestiae necessitatibus nisi, placeat repellat suscipit veritatis voluptas?
                                             Ab, architecto at consequatur dolorem eligendi incidunt laborum pariatur placeat?</p>
@@ -189,7 +189,7 @@
                 </div>
                 <!--/.Slides-->
                 <a class="btn-floating d-flex align-items-center" href="#multi-item-example" data-slide="next">
-                <button class="rounded-circle border border-dark" type="button"  data-bs-slide="next" style="background-color:var(--main-orange)">
+                <button class="rounded-circle border border-dark" type="button"  data-bs-slide="next" >
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Next</span>
                 </button>
