f9cf840d775f016e004f6452bc5f739dc9927f01
AriadnaMiranda95 <77674556+AriadnaMiranda95@users.noreply.github.com>
2022-02-17 12:45:29 +0000

    implementacion imagen/ retoque de contenedor principal

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,9 +8,14 @@
     <h1><span class="sobre">SOBRE </span><span class="nosotros">NOSOTROS</span></h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe placeat odit nobis, ea impedit harum, doloribus eum quia, incidunt minus soluta dolores reprehenderit minima labore vero suscipit. Perferendis, ut aperiam?
     Vitae, neque? Sit totam dolorem architecto itaque voluptate, laboriosam obcaecati fugiat perferendis hic magni voluptatibus distinctio adipisci ea eveniet unde quidem magnam nemo maiores deleniti ab officia asperiores. Reprehenderit, nihil!
-    Quod, animi accusamus aliquid quas iusto magni cupiditate eum. Neque cum dignissimos incidunt ullam. Ipsum nulla laboriosam, nemo, aut exercitationem ad voluptatibus ea sunt vero quod saepe sapiente accusamus perferendis?
-    
     </p>
 </div>
-Contacto
+<div class="info-container">
+<section>
+    
+</section>
+<img src="{{ asset('images/contactos/prueba.png') }}"  alt="">
+
+</div>
+
 @endsection
