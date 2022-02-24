a3b525e25ac26da515e063048d1688f80c5c9394
DanielLorenzo22 <daniellorenzocabrera@gmail.com>
2022-02-24 12:45:52 +0000

    Slider arreglado y svg refactorizado

--- resources/views/layouts/template2.blade.php
+++ resources/views/layouts/template2.blade.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proyecto Fitness</title>
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
     <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@100&family=Open+Sans:wght@300&family=Sora:wght@100&family=Ubuntu:wght@300&display=swap" rel="stylesheet"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"  rel="stylesheet" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
     <link href="{{ asset('css/nav.css') }}" rel="stylesheet"/>
     <script src="{{ asset('js/nav.js') }}"></script>
+
 
 </head>
 <body>
