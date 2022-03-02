132b5396f01c6583473b9f424602d013a2a21235
borjauni <borjauni@hotmail.com>
2022-03-02 20:39:11 +0000

    Carousel Minor Fix

--- resources/views/layouts/template2.blade.php
+++ resources/views/layouts/template2.blade.php
@@ -5,10 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
     <title>Proyecto Fitness</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
 
     <!-- FAVICON -->
     <link rel="shortcut icon" type="image/png" href="{{ asset('/images/favicon.png') }}" />
