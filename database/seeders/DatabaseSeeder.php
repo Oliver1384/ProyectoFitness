597489f2a536bc5e9ef03f1d57d9fa6b5c30f8be
oliver1384 <oliver_artiles@hotmail.com>
2022-03-08 12:41:51 +0000

    ahora al realizar la migración se crea un administrador por defecto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder {
     
     public function run() {
-        $this->call(InformacionCentroSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(PodcastSeeder::class);
         $this->call(PlanSeeder::class);
