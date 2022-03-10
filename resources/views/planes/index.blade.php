09555d60a7628cdfdb815f3c800e28f929ff8163
DanielLorenzo22 <daniellorenzocabrera@gmail.com>
2022-03-10 08:31:30 +0000

    Botones crud planes terminados

--- resources/views/planes/index.blade.php
+++ resources/views/planes/index.blade.php
@@ -25,7 +25,7 @@
 <h1>Bienvenido a la gestión de Planes</h1>
 
 <section class="barraElementos">
-  <a type="button" id="add" href="{{route('planes.create')}}" class="btn btn-primary btn-lg">+ Añadir</a>
+  <a type="button" id="add" href="{{route('planes.create')}}" class="btn btn-primary btn-lg">Añadir</a>
   <form action="{}" method="get">
     <div class="form-row" style="display: flex; justify-content:flex-end; margin: .5em 4em 0 0; ">
       <div class="d-flex" style="max-width: 300px;">
@@ -61,7 +61,7 @@
             <form action="{{ url('/planes/destroy')}}" method="post">
                 @csrf
                 <input type="hidden" name="id" value="{{$plan->id}}" class="btn btn-sm btn-info">
-                <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('¿Desea eliminar el plan?')">Eliminar</button>
+                <input type="submit" class="btn btn-sm btn-danger" onclick="return confirm('¿Desea eliminar el plan?')" value="eliminar">
             </form>
           </div>
         </div>
