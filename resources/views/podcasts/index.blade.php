398515b863dc958d431f310c7a792755918a0f17
oliver <oliver_artiles@hotmail.com>
2022-03-10 19:19:07 +0000

    terminado estilo de paginador

--- resources/views/podcasts/index.blade.php
+++ resources/views/podcasts/index.blade.php
@@ -26,6 +26,7 @@
     @endif
 
     <section class="barraElementos">
+        <h1>Administrar podcasts</h1>
         <a type="button" id="add" href="{{route('agregarPodcast')}}" class="btn btn-primary btn-lg">Añadir</a>
         <form action="{{ url('/admin/podcasts') }}" method="get">
             <div class="form-row" style="display: flex; justify-content:flex-end; margin: .5em 4em 0 0; ">
@@ -40,35 +41,33 @@
     </section>
     <div class="contenedor">
         @foreach($podcasts as $podcast)
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <img src="{{ asset($podcast->imagen) }}" class="card-img-top" alt="imagen relacionada con el podcasts">
-                    <div class="col-md-8">
-                        <div class="card-body">
+            <div class="card mb-3">
+                <img src="{{ asset($podcast->imagen) }}" class="card-img-top" alt="imagen relacionada con el podcasts">
+                    <div class="card-body">
+                        <h2 class="card-title">{{ $podcast->titulo }} <span>{{ $podcast->tema }}</span></h2>
+                        <audio controls preload="none">
+                            <source src="{{ asset($podcast->audio) }}" type="audio/mp3"/>
+                            Tu navegador no soporta el recurso de audio.
+                        </audio>
+                        <p class="card-text">Número de veces escuchado: {{$podcast->veces_escuchado}}</p>
+                        <p class="card-text">{{($podcast->destacado === 1) ? 'Destacado' : 'No destacado'}}</p>
 
-                            <h2 class="card-title">{{ $podcast->titulo }} <span>{{ $podcast->tema }}</span></h2>
-                            <audio controls preload="none">
-                                <source src="{{ asset($podcast->audio) }}" type="audio/mp3" />
-                                Tu navegador no soporta el recurso de audio.
-                            </audio>
-                            <p class="card-text">Número de veces escuchado:{{$podcast->veces_escuchado}}</p>
-                            <p class="card-text">{{($podcast->destacado === 1) ? 'Destacado' : 'No destacado'}}</p>
-
-                            @foreach ($usuarios as $usuario)
-                                @if($usuario->id === $podcast->user_id)
-                                    <p class="card-text">Por {{$usuario->name}}</p>
-                                @endif
-                            @endforeach
-                            <div>
-                                <a class="btn btn-sm btn-primary" href="{{ route('podcasts.edit', $podcast->id) }}">Editar</a>
-                                <form class="inline-block" action="{{ route('podcasts.destroy', $podcast->id) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button onclick="return confirm('¿Estás seguro de querer eliminar el libro?')" type="submit" class="btn btn-sm btn-danger">Eliminar</button>
-                                </form>
-                            </div>
+                        @foreach ($usuarios as $usuario)
+                            @if($usuario->id === $podcast->user_id)
+                                <p class="card-text">Autor: {{$usuario->name}}</p>
+                            @endif
+                        @endforeach
+                        <div class="botonesPodcast">
+                            <a class="btn btn-info btn-sm" href="{{ route('podcasts.edit', $podcast->id) }}">Editar</a>
+                            <form class="inline-block" action="{{ route('podcasts.destroy', $podcast->id) }}"
+                                  method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button onclick="return confirm('¿Estás seguro de querer eliminar el libro?')"
+                                        type="submit" class="btn btn-sm btn-danger">Eliminar
+                                </button>
+                            </form>
                         </div>
-                    </div>
                 </div>
             </div>
         @endforeach
