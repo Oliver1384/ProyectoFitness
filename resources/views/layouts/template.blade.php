9709481cd00e62cbd1c236c0cf0dc9d9356d6f00
DanielLorenzo22 <daniellorenzocabrera@gmail.com>
2022-03-10 09:36:42 +0000

    Redireccion a admin cuando se realiza login

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -105,7 +105,7 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            <a class="dropdown-item" id="logout" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
