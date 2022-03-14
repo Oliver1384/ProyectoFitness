14dbb87cc43b57782bb8c6b61eb237448689026d
AriadnaMiranda95 <77674556+AriadnaMiranda95@users.noreply.github.com>
2022-03-14 11:00:24 +0000

    implementación imagen por defecto para usuarios

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -41,7 +41,7 @@
   <div class="card mb-3" style="max-width: 540px;">
     <div class="row g-0">
       <div class="col-md-4">
-        <img src="{{asset($usuario->imagen)}}" class="img-fluid rounded-start" alt="...">
+        <img src="{{ asset($usuario->imagen) }}" class="img-fluid rounded-start" alt="...">
       </div>
       <div class="col-md-8">
         <div class="card-body">
