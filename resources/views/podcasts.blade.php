511945022455359b3abe7e2f5ca580e646ebf856
oliver1384 <oliver_artiles@hotmail.com>
2022-03-14 11:45:06 +0000

    botones para la version movil de la administracion de podcasts

--- resources/views/podcasts.blade.php
+++ resources/views/podcasts.blade.php
@@ -131,34 +131,5 @@
             >
         </button>
     </div>
-    <script>
-        let iteracion = 0;
-        let podcasts = document.querySelectorAll('.ocultarEscritorio .podcast');
-        let botonAvanzar = document.querySelector('.avanzar');
-        let botonRetroceder = document.querySelector('.retroceder');
-        botonRetroceder.addEventListener('click', restarIteracion);
-        botonAvanzar.addEventListener('click', sumarIteracion);
-        mostrar();
-        function mostrar() {
-            for (let i = 0; i < podcasts.length ; i++) {
-                if (i < iteracion+5 && i >= iteracion) {
-                    podcasts[i].style.display = 'flex';
-                } else {
-                    podcasts[i].style.display = 'none';
-                }
-            }
-        }
-        function restarIteracion() {
-            if (iteracion >= 5 ) {
-                iteracion -= 5;
-                mostrar();
-            }
-        }
-        function sumarIteracion() {
-            if (iteracion+5 <= podcasts.length) {
-                iteracion += 5;
-                mostrar();
-            }
-        }
-    </script>
+    <script src="{{ URL::asset('js/controlesMovil.js') }}"></script>
 @endsection
