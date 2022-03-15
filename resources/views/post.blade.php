71f500781a04f67be89f83482232687bc318b41d
DanielLorenzo22 <daniellorenzocabrera@gmail.com>
2022-03-15 11:30:08 +0000

    Arreglaro responsive de ver post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -66,10 +66,6 @@
                         <hr />
                     </div>
                     <div class="profile">
-                        <div class="profile_avatar">
-                            <img src= "{{asset("{$post->user->imagen}")  }}"
-                                alt="Imagen de perfil del entrenador">
-                        </div>
                         <div class="profile_info">
                             <p class="profile_name">
                                 {{ $post->user->name }}
@@ -78,17 +74,24 @@
                                 {{ $post->user->titulacion }}
                             </p>
                         </div>
-                        <div class="profile_social_media">
-                            <a href="{{ $post->user->instagram }}" target="_blank">
-                                <img src="{{ asset('images/iconos/trainer_instagram.svg') }}" alt="Instagram" />
-                            </a>
-                            <a href="{{ $post->user->facebook }}" target="_blank">
-                                <img src="{{ asset('images/iconos/trainer_facebook.svg') }}" alt="Instagram" />
-                            </a>
-                            <a href="{{ $post->user->linkedin }}" target="_blank">
-                                <img src="{{ asset('images/iconos/trainer_linkedin.svg') }}" alt="Instagram" />
-                            </a>
-                        </div>
+                        
+                            <div class="profile_avatar">
+                                <img src= "{{asset("{$post->user->imagen}")  }}"
+                                    alt="Imagen de perfil del entrenador">
+                            </div>
+                            <div class="profile_social_media">
+                                <a href="{{ $post->user->instagram }}" target="_blank">
+                                    <img src="{{ asset('images/iconos/trainer_instagram.svg') }}" alt="Instagram" />
+                                </a>
+                                <a href="{{ $post->user->facebook }}" target="_blank">
+                                    <img src="{{ asset('images/iconos/trainer_facebook.svg') }}" alt="Instagram" />
+                                </a>
+                                <a href="{{ $post->user->linkedin }}" target="_blank">
+                                    <img src="{{ asset('images/iconos/trainer_linkedin.svg') }}" alt="Instagram" />
+                                </a>
+                            </div>
+
+                        
                     </div>
                 </div>
             </div>
